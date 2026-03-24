5d6b6c24a94aed29e5f23f2ffad02ee6b4f3a9cd
Wiktor-88 <wiktorchmiel67@gmail.com>
2026-03-24 20:41:17 +0100

    Eda, config - narazie koniec, jeszcze później trzeba będzie skleic

--- src/config.py
+++ src/config.py
@@ -5,12 +5,13 @@ DB_PATH = '../hospital_data.db' # Ścieżka względem folderu notebooks
 
 # Palety kolorów
 COLORS = {
-    'primary': '#2C3E50',      # Ciemny granatowy (główny kolor wykresów)
-    'secondary': '#18BC9C',    # Morski/Turkusowy (akcenty)
-    'danger': '#E74C3C',       # Czerwony (do oznaczania np. braków danych)
-    'background': '#ECF0F1',   # Jasnoszary tło
-    'missing_scale': 'Reds',     # Skala gradientowa dla braków danych
-    'correlation_scale': 'RdBu_r'# Do korelacji zmiennych
+    'primary': '#2C3E50',           # Ciemny granatowy (główny kolor wykresów)
+    'secondary': '#18BC9C',         # Morski/Turkusowy (akcenty)
+    'danger': '#E74C3C',            # Czerwony (do oznaczania np. braków danych)
+    'background': '#ECF0F1',        # Jasnoszary tło
+    'missing_scale': 'Reds',          # Skala gradientowa dla braków danych
+    'correlation_scale': 'RdBu_r',    # Do korelacji zmiennych
+    'dist_colors': ['#3498DB', '#E67E22', '#9B59B6']      # Nowa paleta dla KDE i rozkładów
 }
 
 # Wykresy
@@ -21,4 +22,12 @@ PLOT_CONFIG = {
     'label_font_size': 14,
     'width': 900,
     'height': 600
+}
+
+# Dla zmiennych kategorycznych
+CAT_CONFIG = {
+    'high_cardinality_threshold': 10,   # Powyżej tej liczby unikalnych wartości szukamy grup
+    'dominance_threshold': 95.0,        # Jeśli jedna kategoria ma >95%, kolumna jest prawie stała
+    'imbalance_cmap': 'YlOrRd',         # Kolorystyka dla tabeli statystyk
+    'missing_color': '#E74C3C'        # Kolor do podświetlania braków
 }
